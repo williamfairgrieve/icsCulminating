04de179fe5bbbb91acc1f7ad5c16ced95450ed04
William Fairgrieve <william.fairgrieve@student.tdsb.on.ca>
2014-05-26 12:58:30 -0400

    Added slope calc

--- culminating/Form1.Designer.vb
+++ culminating/Form1.Designer.vb
@@ -25,6 +25,9 @@ Partial Class Form1
         Me.closeButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.infoButton = New System.Windows.Forms.Button()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.slopeCalc = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'closeButton
@@ -55,14 +58,43 @@ Partial Class Form1
         Me.infoButton.Text = "INFO"
         Me.infoButton.UseVisualStyleBackColor = True
         '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(480, 449)
+        Me.ShapeContainer1.TabIndex = 3
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 11
+        Me.LineShape1.X2 = 464
+        Me.LineShape1.Y1 = 51
+        Me.LineShape1.Y2 = 51
+        '
+        'slopeCalc
+        '
+        Me.slopeCalc.Location = New System.Drawing.Point(11, 64)
+        Me.slopeCalc.Name = "slopeCalc"
+        Me.slopeCalc.Size = New System.Drawing.Size(97, 23)
+        Me.slopeCalc.TabIndex = 4
+        Me.slopeCalc.Text = "Slope Calculator"
+        Me.slopeCalc.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(480, 449)
+        Me.Controls.Add(Me.slopeCalc)
         Me.Controls.Add(Me.infoButton)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.closeButton)
+        Me.Controls.Add(Me.ShapeContainer1)
         Me.Name = "Form1"
         Me.Text = "Utilities"
         Me.ResumeLayout(False)
@@ -72,5 +104,8 @@ Partial Class Form1
     Friend WithEvents closeButton As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents infoButton As System.Windows.Forms.Button
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents slopeCalc As System.Windows.Forms.Button
 
 End Class
--- culminating/Form1.vb
+++ culminating/Form1.vb
@@ -7,4 +7,23 @@
     Private Sub infoButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles infoButton.Click
         AboutBox1.Show()
     End Sub
+
+    Private Sub slopeCalc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles slopeCalc.Click
+        Dim strFirstCoordinate As String
+        Dim strSecondCoordinate As String
+
+        Dim sinFirstCoordinate(0 To 1) As Single
+        Dim sinSecondCoordinate(0 To 1) As Single
+
+        strFirstCoordinate = InputBox("Enter your first coordinate (x,y): ")
+        strSecondCoordinate = InputBox("Enter your second coordinate (x,y): ")
+
+        sinFirstCoordinate(0) = 666
+        sinFirstCoordinate(1) = 69
+
+        sinSecondCoordinate(0) = 420
+        sinSecondCoordinate(1) = 520
+
+        MsgBox("Coord 1  " & sinFirstCoordinate(0) & sinFirstCoordinate(1) & "Coord 2  " & sinSecondCoordinate(0) & sinSecondCoordinate(1))
+    End Sub
 End Class
